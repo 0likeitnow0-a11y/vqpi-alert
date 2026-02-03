d144a879f18ce313999711cf071e19af7faa6df9
0likeitnow0-a11y <0like.it.now0@gmail.com>
2026-02-03 20:47:46 -0300

    Update dados_fundamentais.py

--- dados_fundamentais.py
+++ dados_fundamentais.py
@@ -1,4 +1,5 @@
 FUNDAMENTOS = {
+
     "BBAS3": {
         "fcf": 80000000000,
         "multiplo_fcf": 10,
@@ -8,6 +9,37 @@ FUNDAMENTOS = {
         "dividendo": 2.4,
         "yield_justo": 0.05
     },
+
+    "BBDC4": {
+        "fcf": 35000000000,
+        "multiplo_fcf": 9,
+        "acoes": 5200000000,
+        "lpa": 3.2,
+        "pl_medio": 11,
+        "dividendo": 1.2,
+        "yield_justo": 0.06
+    },
+
+    "ITSA4": {
+        "fcf": 15000000000,
+        "multiplo_fcf": 10,
+        "acoes": 9400000000,
+        "lpa": 1.5,
+        "pl_medio": 12,
+        "dividendo": 0.9,
+        "yield_justo": 0.05
+    },
+
+    "CPFE3": {
+        "fcf": 8000000000,
+        "multiplo_fcf": 14,
+        "acoes": 1150000000,
+        "lpa": 4.2,
+        "pl_medio": 13,
+        "dividendo": 2.5,
+        "yield_justo": 0.06
+    },
+
     "CSMG3": {
         "fcf": 2500000000,
         "multiplo_fcf": 12,
@@ -16,6 +48,55 @@ FUNDAMENTOS = {
         "pl_medio": 10,
         "dividendo": 3.0,
         "yield_justo": 0.06
+    },
+
+    "UNIP3": {
+        "fcf": 4000000000,
+        "multiplo_fcf": 11,
+        "acoes": 80000000,
+        "lpa": 22,
+        "pl_medio": 8,
+        "dividendo": 6,
+        "yield_justo": 0.07
+    },
+
+    "VULC3": {
+        "fcf": 700000000,
+        "multiplo_fcf": 14,
+        "acoes": 270000000,
+        "lpa": 1.4,
+        "pl_medio": 15,
+        "dividendo": 0.6,
+        "yield_justo": 0.04
+    },
+
+    "LEVE3": {
+        "fcf": 600000000,
+        "multiplo_fcf": 13,
+        "acoes": 140000000,
+        "lpa": 3.2,
+        "pl_medio": 14,
+        "dividendo": 1.1,
+        "yield_justo": 0.04
+    },
+
+    "SLCE3": {
+        "fcf": 900000000,
+        "multiplo_fcf": 10,
+        "acoes": 200000000,
+        "lpa": 4.8,
+        "pl_medio": 11,
+        "dividendo": 1.5,
+        "yield_justo": 0.05
+    },
+
+    "RANI3": {
+        "fcf": 500000000,
+        "multiplo_fcf": 12,
+        "acoes": 180000000,
+        "lpa": 2.3,
+        "pl_medio": 13,
+        "dividendo": 0.8,
+        "yield_justo": 0.05
     }
 }
-
