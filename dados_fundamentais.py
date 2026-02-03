d2fcaff876059e1a9ca1baa7b9d14a99164f0fbd
0likeitnow0-a11y <0like.it.now0@gmail.com>
2026-02-03 19:49:38 -0300

    Update dados_fundamentais.py

--- dados_fundamentais.py
+++ dados_fundamentais.py
@@ -1,1 +1,21 @@
+FUNDAMENTOS = {
+    "BBAS3": {
+        "fcf": 80000000000,
+        "multiplo_fcf": 10,
+        "acoes": 2800000000,
+        "lpa": 8.5,
+        "pl_medio": 9,
+        "dividendo": 2.4,
+        "yield_justo": 0.05
+    },
+    "CSMG3": {
+        "fcf": 2500000000,
+        "multiplo_fcf": 12,
+        "acoes": 390000000,
+        "lpa": 6.2,
+        "pl_medio": 10,
+        "dividendo": 3.0,
+        "yield_justo": 0.06
+    }
+}
 
