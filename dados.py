b00a47368e4fb9ce5faea0cf5d0b35ff2061376f
0likeitnow0-a11y <0like.it.now0@gmail.com>
2026-02-03 19:48:57 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,1 +1,13 @@
 
+ACOES = [
+    "BBAS3", "BBDC4", "ITSA4", "CPFE3", "CSMG3",
+    "UNIP3", "VULC3", "LEVE3", "SLCE3", "RANI3"
+]
+
+PESO_BASE = {acao: 1 / len(ACOES) for acao in ACOES}
+
+MULTIPLICADORES = {
+    "VERDE": 2.0,
+    "AMARELA": 1.0,
+    "VERMELHA": 0.3
+}
